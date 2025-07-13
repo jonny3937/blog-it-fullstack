8d65ec0b7f171a6f9832ad8c5edd4c2384b70fce
Your Name <you@example.com>
2025-07-13 18:00:16 +0300

    client

--- clients/src/services/Api.ts
+++ clients/src/services/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://blog-it-fullstack.onrender.com/api" });
 
 API.interceptors.request.use((config) => {
   const tok = localStorage.getItem("token");
