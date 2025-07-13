a9413213ddc65aefde69341af074d179c95d1e40
Your Name <you@example.com>
2025-07-13 18:06:49 +0300

    added path for frontend to sever

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["https://blog-it-fullstack.vercel.app", "http://localhost:3000"],
     credentials: true,
   }),
 );
